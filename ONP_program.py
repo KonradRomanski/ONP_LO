e598ee9facef55eebf28dbc3df380a4c1fdae1c2
TheSoulsHunter <konrad.romanski4@gmail.com>
2019-11-10 20:19:27 +0100

    Dodanie wczytywania i wypisywania danych

--- ONP_program.py
+++ ONP_program.py
@@ -1,9 +1,31 @@
-def change_to_ONP():
+# Funkcja z pętlą z której aby wyjść podczas wpisywania trzeba nacisnąć Ctrl + d
+def lread(li = []):
+    while(True):
+        try:
+            listt = input().split()
+            li.append(listt)
+
+        except EOFError:
+            break
+    return li
 
 
+def lprint(li):
+    for i in enumerate(li):
+        for j in li[i[0]]:
+            print(j, end=" ")
+        print("")
 
-def main()
+
+def change_to_ONP(li):
     
+    return li
 
+
+
+def main():
+    lists = lread()
+    lists_changed = change_to_ONP(lists)
+    lprint(lists_changed)
 
 main()
