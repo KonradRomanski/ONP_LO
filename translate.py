f1990aa0abbc973b84940af1815a1eec1de2f370
TheSoulsHunter <konrad.romanski4@gmail.com>
2019-11-13 16:49:52 +0100

    Doanie obsługi predykatów i funkcji

--- translate.py
+++ translate.py
@@ -1,6 +1,8 @@
-quantifiers = {"FORALL", "∀","EXISTS", "∃"}
 single_argument = {"NOT", "~", "¬"}
 double_argument = {"AND", "&", "∧", "OR", "|", "∨", "IMPLIES", "→", "IFF", "↔", "XOR", "⊕"}
+quantifiers = {"FORALL", "∀","EXISTS", "∃"}
+functions = {"f", "g", "h", "i", "j", "k", "l", "m", "n"}
+predicats = {"p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
 
 def to_onp(li):
     i = 0
@@ -10,18 +12,24 @@ def to_onp(li):
             i += 1
         # pobiera tyle argumentów, ile mu potrzebne
         if li[i] in single_argument:                                    # operatory 1-argumentowe
-            li[i-1] = li[i] + " " + li.pop(i-1)
-                              # do operatora dopisuje argument, na który operator działa (poprzedni element z tablicy)
-        elif li[i] in quantifiers:
-            print()
+            li[i-1] = f"{li[i]} {li.pop(i-1)}"
+            print("##", li)  # do operatora dopisuje argument, na który operator działa (poprzedni element z tablicy)
 
         elif li[i] in double_argument:  # operatory 2-argumentowe
             arg1 = li.pop(i - 2)                                   # pobiera 2 argumenty (2 poprzednie elementy tablicy)
             arg2 = li.pop(i - 2)
             i -= 2
-            li[i] = f"{arg1} {li[i]} {arg2}"
+            li[i] = f"({arg1} {li[i]} {arg2})"
             i += 1
+            print("##", li)
 
-        else:                                                  # predykaty
+        elif li[i] in quantifiers:
             print()
+
+        elif li[i][0] in predicats or functions:
+            for j in range(int(li[i][2])):
+                li[i-1] = f"{li.pop(i)} {li[i-1]},"
+                i -= 1
+            li[i] = f"{li[i][0]}({li[i][4:len(li[i])-1]})"
+            i += 1
     return li
