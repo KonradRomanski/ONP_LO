7f75d4f94daaca477dd30bacddefd5b26995008d
TheSoulsHunter <konrad.romanski4@gmail.com>
2019-11-13 14:29:01 +0100

    Dodanie możliwości wypisywania danych co linię

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import translate
 
-# Funkcja z pętlą z której aby wyjść podczas wpisywania trzeba nacisnąć Ctrl + d
-def lread(li=[]):
+def read_whole_list_of_lists(li=[]):
     while (True):
         try:
-            listt = input().split()
-            li.append(listt)
+            list_temp = input().split()
+            li.append(list_temp)
 
         except EOFError:
             print()
@@ -17,17 +16,36 @@ def lread(li=[]):
     return li
 
 
-def lprint(li):
+def print_whole_list_of_lists(li):
     for i in li:
         for j in i:
             print(j, end=" ")
         print()
 
+def read_each_line_separetly():
+    while (True):
+        try:
+            list_temp = input().split()
+            list_temp = translate.to_onp(list_temp)
+            for i in list_temp: print(i, end=" ")
+            print()
+
+        except EOFError:
+            print()
+            break
+
+        except KeyboardInterrupt:
+            print("\n")
+            break
+
 
 def main():
-    lists = lread()
-    lists_changed = translate.to_onp(lists)
-    lprint(lists_changed)
+    ## reading everything and then printing
+    # lists = read_whole_list_of_lists()
+    # lists_changed = translate.to_onp(lists)
+    # print_whole_list_of_lists(lists_changed)
 
+    ## reding each line separetly
+    read_each_line_separetly()
 
 main()
