1b0ffa2aa24519d2d3e3d68645885820baaf64fc
TheSoulsHunter <konrad.romanski4@gmail.com>
2019-11-20 01:38:28 +0100

    add "-l" flag handling

--- main.py
+++ main.py
@@ -6,38 +6,19 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("-p", "--pikatchu", action='store_true')
 parser.add_argument("-l", "--lines", action='store_true')
+lili = parser.parse_args().lines
+piki = parser.parse_args().pikatchu
 
 def show_image(message):
     a = open("error_art.txt", 'r', encoding="utf-8").read()
     a = f"\033[1;31m  {message[:-5].upper()}  {a}\033[0m"
     return a
 
-def read_whole_list_of_lists(li=[]):
-    while (True):
-        try:
-            list_temp = input().split()
-            li.append(list_temp)
-
-        except EOFError:
-            print()
-            break
-
-        except KeyboardInterrupt:
-            print("\n")
-            break
-        except (ValueError, IndexError):
-            print(show_image())
-            break
-    return li
-
-
 def print_whole_list_of_lists(li):
     for i in li:
-        for j in i:
-            print(j, end=" ")
-        print()
+        print(f"\033[33;1m{i}\033[0m")
 
-def read_each_line_separetly(): # def read_each_line_separetly(list_temp):
+def read_data(li = []): # def read_data(list_temp):
     while (True):
         try:
             # if list_temp == []:
@@ -47,32 +28,35 @@ def read_each_line_separetly(): # def read_each_line_separetly(list_temp):
                 # textpika.goodbye(language)
                 # break
             list_temp = onp_translation.to_onp(list_temp)
-            # list_temp = []
-            print(f"\033[33;1m{list_temp}\033[0m")
+
+            if lili == False: print(f"\033[33;1m{list_temp}\033[0m")
+            else: li.append(list_temp)
 
         except EOFError:
             print("\033[0m")
+            if lili == True: return li
             break
 
         except KeyboardInterrupt:
             print("\033[0m\n")
+            if lili == True: return li
             break
         except (ValueError, IndexError) as Error:
             print("\033[0m", end="")
             print(show_image(sys.exc_info()[0].__name__), end="")
             # print(show_image(type(Error).__name__))
-            # break
 
 def main():
-    #print(parser.parse_args().pikatchu)
+    #print(piki)
 
-    ## reading everything and then printing
+    # reading everything and then printing
     # lists = read_whole_list_of_lists()
     # lists_changed = onp_translation.to_onp(lists)
     # print_whole_list_of_lists(lists_changed)
 
     ## reding each line separetly
-    read_each_line_separetly()
+    read_data()
+    if lili == True: print_whole_list_of_lists(read_data())
 
 if __name__ == "__main__":
     main()
